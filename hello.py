e257c801e8a42da22626971a827f2829e4c168b1
Samu5706k <samu5706k@gmail.com>
2026-06-03 09:00:08 -0400

    git push

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print('hola mundo')
+print('saldo actual:1000')
+cantidad = 1000
+cantidad = int(input('ingrese la cantidad que quiere retirar: '))
+if cantidad < 1000:
+    print('retiro exitoso')
+    cantidad = 1000 - cantidad
+    print('saldo restante: ', cantidad)
+else:
+    print('error, fondos insuficientes')
+    
